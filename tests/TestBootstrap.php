8331887cf06ebaf72dc9b3304c174c8248cd11c3
Abdurahman Nigmetov <nigmetovabdurahman@gmail.com>
2026-08-11 18:06:15 +0500

    test: activate project plugins in test bootstrap

--- tests/TestBootstrap.php
+++ tests/TestBootstrap.php
@@ -2,9 +2,19 @@
 
 declare(strict_types=1);
 
+use Shopware\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
 use Shopware\Core\TestBootstrapper;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
 
-(new TestBootstrapper())
+$bootstrapper = (new TestBootstrapper())
     ->setPlatformEmbedded(false)
-    ->addActivePlugins('JvMarketConfiguration', 'JvCms', 'JvImport')
-    ->bootstrap();
+    ->addActivePlugins('JvMarketConfiguration', 'JvCms', 'JvImport');
+
+$bootstrapper->bootstrap();
+
+(new Application(KernelLifecycleManager::getKernel()))->doRun(
+    new ArrayInput(['command' => 'cache:clear', '--no-warmup' => true, '--no-interaction' => true]),
+    new \Symfony\Component\Console\Output\NullOutput(),
+);
+KernelLifecycleManager::ensureKernelShutdown();
